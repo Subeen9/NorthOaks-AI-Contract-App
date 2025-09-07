5332d05768f588536ce09084dd00ed341beb3933
subin <subin.bista@selu.edu>
2025-09-07 13:23:41 -0500

    Add local TokenService

---
+++ Data/Services/TokenService.cs
@@ -0,0 +1,41 @@
+﻿using CMPS4110_NorthOaksProj.Models.Users;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace CMPS4110_NorthOaksProj.Data.Services
+{
+    public class TokenService
+    {
+        private readonly IConfiguration config;
+
+        public TokenService(IConfiguration _config)
+        {
+            config = _config;
+        }
+
+        public string GenerateToken(User user) 
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
+                new Claim(JwtRegisteredClaimNames.Email, user.Email),
+                new Claim("uid", user.Id.ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: config["Jwt:Issuer"],
+                audience: config["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddHours(1),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
