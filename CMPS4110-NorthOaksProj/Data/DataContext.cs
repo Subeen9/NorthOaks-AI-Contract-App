79c937821bb148e6429f5e6910fa3ace9b88fb99
Anuraj235 <pantanuraj31@gmail.com>
2025-09-10 18:23:18 -0500

    datacontext update

--- CMPS4110-NorthOaksProj/Data/DataContext.cs
+++ CMPS4110-NorthOaksProj/Data/DataContext.cs
@@ -1,6 +1,8 @@
-﻿using CMPS4110_NorthOaksProj.Models.Users;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using CMPS4110_NorthOaksProj.Models.Chat;
+using CMPS4110_NorthOaksProj.Models.Users;
+using CMPS4110_NorthOaksProj.Models.Contracts;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
@@ -10,7 +12,11 @@ namespace CMPS4110_NorthOaksProj.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         public DbSet<User> Users { get; set; }
+        public DbSet<Contract> Contracts { get; set; }
 
+        public DbSet<ChatSession> ChatSessions { get; set; } = null!;
+        public DbSet<ChatMessage> ChatMessages { get; set; } = null!;
+        public DbSet<ChatSessionContract> ChatSessionContracts { get; set; } = null!;
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
