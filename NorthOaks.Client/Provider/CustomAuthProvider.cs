f8b9c0d128c2a858584e23612e1dcf39adcf60b0
subin <subin.bista@selu.edu>
2025-09-13 14:55:02 -0500

    Added Auth Flow in Frontend.

---
+++ NorthOaks.Client/Provider/CustomAuthProvider.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.JSInterop;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+public class CustomAuthStateProvider : AuthenticationStateProvider
+{
+    private readonly IJSRuntime _jsRuntime;
+
+    public CustomAuthStateProvider(IJSRuntime jsRuntime)
+    {
+        _jsRuntime = jsRuntime;
+    }
+
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var token = await _jsRuntime.InvokeAsync<string>("localStorage.getItem", "authToken");
+
+        if (string.IsNullOrWhiteSpace(token))
+            return new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity())); // not logged in
+
+        var jwt = new JwtSecurityTokenHandler().ReadJwtToken(token);
+
+        var identity = new ClaimsIdentity(jwt.Claims, "jwtAuth");
+        var user = new ClaimsPrincipal(identity);
+
+        return new AuthenticationState(user);
+    }
+
+    public void NotifyUserAuthentication(string token)
+    {
+        var jwt = new JwtSecurityTokenHandler().ReadJwtToken(token);
+        var identity = new ClaimsIdentity(jwt.Claims, "jwtAuth");
+        var user = new ClaimsPrincipal(identity);
+
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(user)));
+    }
+
+    public void NotifyUserLogout()
+    {
+        var anonymous = new ClaimsPrincipal(new ClaimsIdentity());
+        NotifyAuthenticationStateChanged(Task.FromResult(new AuthenticationState(anonymous)));
+    }
+}
