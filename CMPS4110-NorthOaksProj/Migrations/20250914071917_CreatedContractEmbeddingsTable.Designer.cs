695de72f78f617fe0beb5cd831c217dbd1aa8324
Aakash Poudel <aakash.poudel.900@gmail.com>
2025-09-14 02:19:49 -0500

    Embeddings Table Migration

--- CMPS4110-NorthOaksProj/Migrations/20250914071917_CreatedContractEmbeddingsTable.Designer.cs
+++ CMPS4110-NorthOaksProj/Migrations/20250914071917_CreatedContractEmbeddingsTable.Designer.cs
@@ -4,6 +4,7 @@ using CMPS4110_NorthOaksProj.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CMPS4110_NorthOaksProj.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20250914071917_CreatedContractEmbeddingsTable")]
+    partial class CreatedContractEmbeddingsTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ CMPS4110-NorthOaksProj/Migrations/20250914071917_CreatedContractEmbeddingsTable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CMPS4110_NorthOaksProj.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreatedContractEmbeddingsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ContractEmbedding",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ContractId = table.Column<int>(type: "int", nullable: false),
+                    ChunkText = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ChunkIndex = table.Column<int>(type: "int", nullable: false),
+                    EmbeddingVector = table.Column<string>(type: "vector(384)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContractEmbedding", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ContractEmbedding_Contracts_ContractId",
+                        column: x => x.ContractId,
+                        principalTable: "Contracts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContractEmbedding_ContractId",
+                table: "ContractEmbedding",
+                column: "ContractId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContractEmbedding");
+        }
+    }
+}
