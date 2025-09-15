89a4ae2fb72e248434ccf414fc9c395fa8e6619a
Anuraj235 <pantanuraj31@gmail.com>
2025-09-14 22:22:06 -0500

    added contracreaddto to shared

---
+++ NorthOaks.Shared/Model/Contract/ContractReadDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthOaks.Shared.Model.Contracts
+{
+
+
+    public class ContractReadDto
+    {
+        public int Id { get; set; }
+        public string FileName { get; set; }
+        public DateTime UploadDate { get; set; }
+        public int UserId { get; set; }
+        public string UploadedBy { get; set; }
+    }
+}
