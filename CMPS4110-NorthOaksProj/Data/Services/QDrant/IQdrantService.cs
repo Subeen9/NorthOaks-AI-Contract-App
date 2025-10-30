a2cac6ff98c1ba6398ea43f2926f2284ebc34bed
pathak-satyam <satyampathak8059@gmail.com>
2025-10-30 02:14:11 -0500

    "update QdrantService to support pageNumber in vector storage and retrieval"

--- CMPS4110-NorthOaksProj/Data/Services/QDrant/IQdrantService.cs
+++ CMPS4110-NorthOaksProj/Data/Services/QDrant/IQdrantService.cs
@@ -4,7 +4,7 @@ namespace CMPS4110_NorthOaksProj.Data.Services.QDrant
 {
     public interface IQdrantService
     {
-        Task<Guid> InsertVectorAsync(float[] embedding, int contractId, int chunkIndex, string chuntText);
+        Task<Guid> InsertVectorAsync(float[] embedding, int contractId, int chunkIndex, string chuntText, int pageNumber);
         Task<List<VectorSearchResult>> SearchSimilarAsync(float[] queryEmbedding, int limit = 20, float scoreThreshold = 0.15f);
         Task DeleteVectorsByContractAsync(int contractId);
     }
