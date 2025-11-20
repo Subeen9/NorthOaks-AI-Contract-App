6196345888f2406fd59324b85af9dad458945e4c
subin <bistasubeen715@gmail.com>
2025-11-20 14:19:59 -0600

    Add migraiton and Added Enum type for ChatSession

--- CMPS4110-NorthOaksProj/Migrations/20251120172918_AddChatSessionType.Designer.cs
+++ CMPS4110-NorthOaksProj/Migrations/20251120172918_AddChatSessionType.Designer.cs
@@ -4,6 +4,7 @@ using CMPS4110_NorthOaksProj.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CMPS4110_NorthOaksProj.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20251120172918_AddChatSessionType")]
+    partial class AddChatSessionType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- CMPS4110-NorthOaksProj/Models/Chat/ChatSession.cs
+++ CMPS4110-NorthOaksProj/Models/Chat/ChatSession.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 
 namespace CMPS4110_NorthOaksProj.Models.Chat
 {
+    public enum ChatSessionType
+    {
+        Single = 0,
+        Comparison = 1
+    }
+
     public class ChatSession
     {
         public int Id { get; set; }
         public int UserId { get; set; }
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
 
-        public ICollection<ChatMessage> Messages { get; set; } = new List<ChatMessage>();
+        public ChatSessionType SessionType { get; set; } = ChatSessionType.Single;
         public ICollection<ChatSessionContract> SessionContracts { get; set; } = new List<ChatSessionContract>();
+        public ICollection<ChatMessage> Messages { get; set; } = new List<ChatMessage>();
     }
 }
